f8b866bd58405faf939b9cbf343cf1867efa0350
Abbie Eastman <a.eastman@wustl.edu>
2024-08-19 14:20:38 -0500

    message

---
+++ Day04/Lab/Lab ALEastman.py
@@ -0,0 +1,48 @@
+#Fill in the following methods for the class 'Clock'
+class Clock:
+    def __init__(self, hour, minutes):
+        self.minutes = minutes
+        self.hour = hour
+
+    ## Print the time
+    def __str__(self):
+        return self.hour % self.minutes
+    
+    ## Add time
+    ## Don't return anything
+    def __add__(self,minutes):
+        # YOUR CODE HERE
+    
+    ## Subtract time
+    ## Don't return anything
+    def __sub__(self,minutes):
+        # YOUR CODE HERE
+    
+    ## Are two times equal?
+    def __eq__(self, other):
+        # YOUR CODE HERE
+    
+    ## Are two times not equal?
+    def __ne__(self, other):
+        # YOUR CODE HERE
+
+# You should be able to run these
+clock1 = Clock(23, 5)
+print(clock1) # 23:05
+clock2 = Clock(12, 45)
+print(clock2) # 12:45
+clock3 = Clock(12, 45)
+print(clock3) # 12:45
+
+print(clock1 == clock2) ## False
+print(clock1 != clock2) ## True
+print(clock2 == clock3) ## True
+
+print("testing addition")
+clock1 + 60
+print(clock1) # 00:05
+print(clock1 == Clock(0, 5)) # True
+
+print("testing subtraction")
+clock1 - 100
+print(clock1) # 22:25
